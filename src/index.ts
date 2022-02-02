a820ecea2e7101cc289db03c615ab0029a246c0c
Technodrome <junttila.eero@gmail.com>
2022-02-02 16:38:15 +0200

    logTotalTime support

--- src/index.ts
+++ src/index.ts
@@ -4,34 +4,55 @@ export class DurationLogger {
   durationWarningTresholdInMs: number;
   _warningTxt = "!!!!!!!!!!!!!!!";
   _defaultDurationWarningTresholdInMs = 1;
-  id: string | undefined;
-  constructor(id?: string, durationWarningTresholdInMs?: number) {
-    console.log(`Perfomance monitor started ${id ? "for " + id : undefined}`);
-    this.id = id;
+  mainId: string | undefined;
+  logTotalTime: boolean;
+  startTime: number;
+
+  constructor(
+    mainId?: string,
+    durationWarningTresholdInMs?: number,
+    logTotalTime?: boolean
+  ) {
+    console.log(
+      `Perfomance monitor started ${mainId ? "for " + mainId : undefined}`
+    );
+    this.mainId = mainId;
     this.prevTime = this.now();
+    this.startTime = this.prevTime;
     this.index = 0;
+    this.logTotalTime = logTotalTime || false;
     this.durationWarningTresholdInMs =
       durationWarningTresholdInMs ?? this._defaultDurationWarningTresholdInMs;
   }
+
   now() {
     return new Date().getTime();
   }
+
   formatDuration(duration: number) {
     return Math.floor(duration);
   }
-  report(target?: string) {
+
+  log(checkpointId?: string) {
     const now = this.now();
     const duration = this.formatDuration(now - this.prevTime);
-    const msg = `${this.id ? "[" + this.id + "]" : ""}[${this.index}]${
-      target ? "[" + target + "]" : ""
-    } Duration since last checkpoint: ${duration}ms`;
+    const totalDuration = this.formatDuration(now - this.startTime);
+    const msg = `${this.mainId ? "[" + this.mainId + "]" : ""}[${this.index}]${
+      checkpointId ? "[" + checkpointId + "]" : ""
+    } Duration since last checkpoint: ${duration}ms. ${
+      this.logTotalTime ? "Total time elipsed: " + totalDuration : ""
+    }`;
+
     console.log(msg);
+
+    // Add warning log if duration over limit
     duration > this.durationWarningTresholdInMs &&
       console.warn(
         this._warningTxt +
           " Warning, duration over treshold! " +
           this._warningTxt
       );
+
     this.prevTime = now;
     this.index += 1;
   }
