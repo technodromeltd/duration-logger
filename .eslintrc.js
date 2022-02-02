fd43001e5fbde2e1275b16801f922e638c7bcbea
Technodrome <junttila.eero@gmail.com>
2022-02-02 16:37:33 +0200

    eslint fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,16 @@
-export default {
-    "env": {
-        "browser": false,
-        "es2021": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended"
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "@typescript-eslint"
-    ],
-    "rules": {
-    }
-}
+module.exports = {
+  env: {
+    browser: false,
+    es2021: false,
+    commonjs: true,
+    node:true,
+  },
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {},
+};
